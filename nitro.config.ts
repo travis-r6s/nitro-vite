9e4d5fb05f494065d502f30147edd9e83124dafd
Travis Reynolds <travis@travisreynolds.dev>
2024-01-17 12:57:03 +0000

    rm unused config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,6 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   renderer: './renderer.ts',
-  devProxy: {
-    // '**': 'http://localhost:5173'
-  },
   publicAssets: [
     {
       dir: 'dist',
